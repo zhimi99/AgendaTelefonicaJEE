744bcd4f3c1ac51cc2f78c83b52dd90268c1fe9e
zhimi99 <52504869+zhimi99@users.noreply.github.com>
2021-05-06 22:29:08 -0400

    model

--- src/ec/edu/ups/Telefono.java
+++ src/ec/edu/ups/Telefono.java
@@ -8,5 +8,42 @@ public class Telefono implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private int codigo;
+	private String numero;
+	private String tipo;
+	private String operadora;
+	private Usuario usuCedula;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getOperadora() {
+		return operadora;
+	}
+	public void setOperadora(String operadora) {
+		this.operadora = operadora;
+	}
+	public Usuario getUsuCedula() {
+		return usuCedula;
+	}
+	public void setUsuCedula(Usuario usuCedula) {
+		this.usuCedula = usuCedula;
+	}
+	
 
 }
--- src/ec/edu/ups/Usuario.java
+++ src/ec/edu/ups/Usuario.java
@@ -10,7 +10,48 @@ public class Usuario  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	
-	//private
-	//
-
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String clave;
+	
+	
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
 }
