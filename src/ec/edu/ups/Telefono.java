5904b3fb09a5e69417d9810375a141106f3d6375
zhimi99 <52504869+zhimi99@users.noreply.github.com>
2021-05-06 21:15:50 -0400

    developed of model

--- src/ec/edu/ups/Telefono.java
+++ src/ec/edu/ups/Telefono.java
@@ -2,15 +2,11 @@ package ec.edu.ups;
 
 import java.io.Serializable;
 
-public class Usuario  implements Serializable{
+public class Telefono implements Serializable{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
-	//private
-	//
 
 }
